3bf8bc8dbfcb21f86ea9d84cb87798ac8d9c214d
Ehinola kingsley Ayodeji <ehinolakingsleyayodeji@yahoo.com>
2024-12-05 00:27:41 -0500

    pre

---
+++ CSCI-C-308-PROJECT/Extensions/DBExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace CSCI_308_TEAM5.API.Extensions
+{
+    public static class DBExtensions
+    {
+        public static string getInsertQuery<T>(this string tableName)
+        {
+            var properties = typeof(T).GetProperties();
+
+            if (properties is null)
+                throw new ArgumentNullException("No properties found in the selected model.");
+
+            var columns = string.Join(", ", properties.Select(prop => prop.Name));
+            var values = string.Join(", ", properties.Select(prop => $"@{prop.Name}"));
+
+            return $"INSERT INTO {tableName} ({columns}) VALUES ({values});";
+        }
+    }
+}
---
+++ CSCI-C-308-PROJECT/Repository/Authentication/Query.cs
@@ -0,0 +1,12 @@
+﻿using CSCI_308_TEAM5.API.Extensions;
+
+namespace CSCI_308_TEAM5.API.Repository.Authentication
+{
+    static class Query
+    {
+        const string TableName = "";
+
+        internal static string Insert => TableName.getInsertQuery<AuthenticationTbModel>();
+
+    }
+}
